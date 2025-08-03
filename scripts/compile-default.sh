b396462ea6145e30cf299c716b4d76e2e913cd1d
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2025-08-03 12:48:27 +0200

    Dev env from old repo -> main (#21)
    * [add] Dev env from old repo
    
    * <fix>[workflows] Updated branchs name
    
    * <fix>[workflows] run test steps
    
    * <fix>[workflows] run test steps
    
    * <fix>[workflows] run test steps
    
    * <fix>[workflows] run test steps
    
    * [fix] Running workflows on PR (#23)

---
+++ scripts/compile-default.sh
@@ -0,0 +1,48 @@
+##
+## Hylozoa Engine
+## compile-default.sh
+## Compiles Hylozoa with the default settings
+## Must be run at root
+##
+##
+COLOR_NC='\e[0m'
+COLOR_RED='\e[0;31m'
+
+## Errors handling
+# No CMakeLists
+if [[ ! -f "./CMakeLists.txt" ]];
+then
+    echo "$COLOR_RED""Error: No 'CMakeLists.txt' on current directory: aborting...""$COLOR_NC"
+    exit 84
+fi
+# No build dir
+if [[ ! -d "./build/" ]];
+then
+    mkdir build/ --verbose
+    if [[ ! $? ]];
+    then
+        echo -e "$COLOR_RED""Error: Cannot create 'build/' directory.""$COLOR_NC"
+        exit 84
+    fi
+fi
+
+#Script
+cd build/
+cmake .. -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Debug
+if [[ $? -ne 0 ]];
+then
+    echo "$COLOR_RED""An error occured when building with cmake..""$COLOR_NC"
+    exit 84
+fi
+
+make
+if [[ $? -ne 0 ]];
+then
+    echo "$COLOR_RED""An error occured when compiling...""$COLOR_NC"
+
+    exit 84
+fi
+
+echo -e "Success: check the build folder.\nClearning current path..."
+mv --verbose hylozoa.exe ..
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+// EPITECH PROJECT, 2025
+// Hylozoa-Engine
+// File description:
+// main
+//
+
+#include <iostream>
+
+int main(int ac, char * const *av)
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
