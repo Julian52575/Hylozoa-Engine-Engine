54f4400bdd8ddc9501fbe1b91e0905173767445d
julian <julian.bottiglione@epitech.eu>
2025-09-07 16:29:40 +0900

    [add] Creating flecs world

---
+++ src/Hylozoa-Engine/Systems/HelloWorld/HelloWorld.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** Hylozoa Engine
+** File description:
+** Hello World system (temporary)
+*/
+
+#ifndef HELLOWORLD_HPP_
+#define HELLOWORLD_HPP_
+#include <iostream>
+#include <flecs.h>
+
+namespace Hylozoa {
+class HelloWorld {
+    public:
+        HelloWorld(flecs::world& world)
+        {// System declaration
+            sys = world.system("Hello World")
+            .each([](void) {
+                std::cout << "Hello World from Flecs ECS." << std::endl;
+            });
+        };
+        ~HelloWorld() = default;
+        void run()
+        {
+            sys.run();
+        };
+
+    protected:
+    private:
+        flecs::system sys;
+};
+} // namespace Hylozoa
+
+#endif /* !HELLOWORLD_HPP_ */
