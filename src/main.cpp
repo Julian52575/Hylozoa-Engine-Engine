ef7ac3c246870421a1542c9aee4004d53ddb42ea
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2025-09-07 17:08:05 +0900

    Julian flecs addition to the project (#48)
    * [add] Downloading and linking Flecs
    
    * [add] Creating flecs world
    
    * Adding ssh key to download flecs dependencies
    
    * Update tests.yml
    
    * Update tests.yml
    
    * [fix] Format

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,17 @@
 //
 
 #include "Hylozoa-Engine/Placeholder/Placeholder.hpp"
+#include "Hylozoa-Engine/Systems/HelloWorld/HelloWorld.hpp"
+#include <flecs.h>
 #include <iostream>
 
 int main(int ac, char *const *av) {
+  flecs::world world;
   Hylozoa::Placeholder pl;
+  Hylozoa::HelloWorld helloWorldSystem(world);
 
   std::cout << "Hello world from Hylozoa Game Engine main." << std::endl;
   pl.helloWorld();
+  helloWorldSystem.run();
   return 0;
 }
