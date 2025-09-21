aa243b42d83d0697bceda098894508aab5855420
Julian52575 <julian.bottiglione@epitech.eu>
2025-09-22 05:35:53 +0900

    <fix>[nix] Downloading Flecs with Nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,24 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
+
+  flecs = pkgs.stdenv.mkDerivation rec {
+    pname = "flecs";
+    version = "4.1.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "SanderMertens";
+      repo = "flecs";
+      rev = "v${version}"; # Use the version variable defined above
+      sha256 = "sha256-721aci8IU2/14wn3Ysp+wj7627yTY8/8A403CicUijw="; # Update this hash when the version changes
+    };
+    nativeBuildInputs = [ pkgs.cmake pkgs.clang ];
+    buildInputs = [ ];
+    cmakeFlags = [
+      "-DFLECS_SHARED=ON"   # build shared library
+      "-DFLECS_STATIC=OFF"  # disable static lib (optional)
+    ];
+  };
+
 in
 
 pkgs.mkShellNoCC {
@@ -14,6 +32,7 @@ pkgs.mkShellNoCC {
     just
     doxygen
     graphviz
+    flecs
   ];
 
   # Env variables bellow
