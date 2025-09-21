49261be7ad4806b716c6b4a72dbd19b957d548dd
Julian52575 <julian.bottiglione@epitech.eu>
2025-09-22 06:25:46 +0900

    <wip>[nix] Errors when compiling SDL3

--- shell.nix
+++ shell.nix
@@ -19,6 +19,37 @@ let
     ];
   };
 
+  sdl3 = pkgs.stdenv.mkDerivation rec {
+    pname = "sdl3";
+    version = "3.2.22";
+    src = pkgs.fetchFromGitHub {
+      owner = "libsdl-org";
+      repo = "SDL";
+      rev = "release-${version}"; # Use the version variable defined above
+      sha256 = "sha256-4jGfw2hNZTGuae2DMLz8xJBtfNu5abIN5GlNIKDOUpw="; # Update this hash when the version changes
+    };
+    nativeBuildInputs = [ pkgs.cmake pkgs.clang ];
+    buildInputs = [ [
+            pkgs.xorg.libX11
+            pkgs.xorg.libXext
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXScrnSaver
+            pkgs.xorg.libXi
+            pkgs.wayland
+            pkgs.wayland-protocols
+            pkgs.libxkbcommon
+            pkgs.alsa-lib
+            pkgs.pulseaudio
+            pkgs.pipewire
+            pkgs.dbus
+            pkgs.libusb1 ] ];
+    #cmakeFlags = [
+    #  "-DSDL_SHARED=ON"   # build shared library
+    #  "-DSDL_STATIC=OFF"  # disable static lib (optional)
+    #];
+  };
+
 in
 
 pkgs.mkShellNoCC {
@@ -33,6 +64,22 @@ pkgs.mkShellNoCC {
     doxygen
     graphviz
     flecs
+    #SDL and dependencies
+    xorg.libX11
+    xorg.libXext
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXScrnSaver
+    xorg.libXi
+    wayland
+    wayland-protocols
+    libxkbcommon
+    alsa-lib
+    pulseaudio
+    pipewire
+    dbus
+    libusb1
+    sdl3
   ];
 
   # Env variables bellow
