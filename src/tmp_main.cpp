6488a479abc4d7a76bc3691e64a0ac7b7331f380
antoine <antoine.weisse@epitech.eu>
2025-09-23 20:20:37 -0400

    [add] file to compare

---
+++ src/tmp_main.cpp
@@ -0,0 +1,23 @@
+//
+// EPITECH PROJECT, 2025
+// Hylozoa-Engine
+// File description:
+// main
+//
+
+#include "Hylozoa-Engine/Placeholder/Placeholder.hpp"
+#include "Hylozoa-Engine/Systems/HelloWorld/HelloWorld.hpp"
+#include <flecs.h>
+#include <iostream>
+#include "Hylozoa-Engine/SDL/SDL.hpp"
+
+int main(int ac, char *const *av) {
+  flecs::world world;
+  Hylozoa::Placeholder pl;
+  Hylozoa::HelloWorld helloWorldSystem(world);
+  
+  std::cout << "Hello world from Hylozoa Game Engine main." << std::endl;
+  pl.helloWorld();
+  helloWorldSystem.run();
+  return 0;
+}
