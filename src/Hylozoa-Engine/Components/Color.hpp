03692266684e0f3ea845a5b496dc12ac8fa942a0
Julian52575 <julian.bottiglione@epitech.eu>
2025-10-23 21:56:08 +0900

    fix<tidy>

--- src/Hylozoa-Engine/Components/Color.hpp
+++ src/Hylozoa-Engine/Components/Color.hpp
@@ -11,19 +11,19 @@
 namespace Hylozoa::Components {
 
 /**
-* @addtogroup Rendering
-* @namespace Hylozoa::Components
-* @class Color
-* @brief A structure representing RGBA color.
-*/
+ * @addtogroup Rendering
+ * @namespace Hylozoa::Components
+ * @class Color
+ * @brief A structure representing RGBA color.
+ */
 struct Color {
   uint8_t red{255};
   uint8_t green{255};
   uint8_t blue{255};
   /*
-  * @brief The alpha (transparency) value of the color.
-  * 0 = fully visible, 255 = fully transparent.
-  */
+   * @brief The alpha (transparency) value of the color.
+   * 0 = fully visible, 255 = fully transparent.
+   */
   uint8_t alpha{0};
 };
 
--- src/Hylozoa-Engine/Components/Vector2.hpp
+++ src/Hylozoa-Engine/Components/Vector2.hpp
@@ -11,10 +11,11 @@
 
 namespace Hylozoa::Components {
 
-    using Vector2 = glm::vec2;
-    
+using Vector2 = glm::vec2;
+
 } // namespace Hylozoa::Components
 
-inline std::ostream& operator<<(std::ostream& os, const Hylozoa::Components::Vector2& v) {
-    return os << "Vector2(" << v.x << ", " << v.y << ")";
+inline std::ostream &operator<<(std::ostream &os,
+                                const Hylozoa::Components::Vector2 &v) {
+  return os << "Vector2(" << v.x << ", " << v.y << ")";
 }
