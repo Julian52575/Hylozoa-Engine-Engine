e03ed55f2a856f021dba70150d05ab481c6e8bcb
Julian52575 <julian.bottiglione@epitech.eu>
2026-03-31 21:49:08 +0900

    <fix> Using benchmark::DoNotOptimize(entity) and engine settings

--- benchmarks/src/Entity.cpp
+++ benchmarks/src/Entity.cpp
@@ -1,15 +1,24 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
+#include <format>
 #include <Hylozoa-Engine/Core/Engine.hpp>
+
 #include "Hacks.hpp"
 
 static void BM_CreateEntity(benchmark::State& state) {
-    Hylozoa::Engine engine(Hylozoa::EngineMode::HEADLESS);
-    // Perform setup here
+    auto name = "BM_CreateEntity";
+    auto jsonStr = std::format(R"({{
+        "name": "{0}",
+        "verbose": false,
+        "debugLevel": 0
+    }})", name);
+    std::istringstream jsonStream(jsonStr);
+    Hylozoa::Engine engine(Hylozoa::EngineMode::HEADLESS, jsonStream);
+
     for (auto _ : state) {
       auto entity = engine.scene().spawnEntity("BM_Entity");
 
-      //escape(&entity); // Prevent compiler optimizations
+      benchmark::DoNotOptimize(entity); // Prevent compiler optimizations
     }
 }
 // Register the function as a benchmark
